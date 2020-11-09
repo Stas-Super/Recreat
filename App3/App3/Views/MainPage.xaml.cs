8f141e53698fa044db71c6091f2b246cda9208d5
Stas-Super <49335763+Stas-Super@users.noreply.github.com>
2020-11-09 14:58:11 +0200

    Добавьте файлы проекта.

---
+++ App3/App3/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App3;
+using System;
+using Xamarin.Forms;
+
+namespace FacebookLogin.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            faceBook.Clicked += LoginWithFacebook_Clicked;
+        }
+
+        private async void LoginWithFacebook_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FacebookProfilePage());
+        }
+    }
+}
